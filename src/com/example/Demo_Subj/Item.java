348fc43c0c4863490ebda1c2da7ea86600320631
FPS-Doug <mcgeiver@gmx.de>
2014-12-10 17:07:53 +0100

    Room.java-----------------------------------------
    -added parameter in instancing of object Item
    -outcommented the instancing of item-object
    
    Item.java-----------------------------------------
    
    -added comments in class - head   and in general
    
    modified constructor:
    -added range-check for values of x and y - variables
    -added parameter "id"
    -changed Bitmap bitmap -----> String itemStandBitmapName
    
    added attributes:
      -active_state (int)
      -id (int)
      -holdAnimationCycles (int)
      -itemAnim (List<String>)
      -itemAnimInv (List<String>)
    
     modified attribute: Bitmap bitmapO ---> String itemStandBitmapName
    
     added methods:
     -getID
     -useBySubject
     -useByUser
     -tick
    - addResource

--- src/com/example/Demo_Subj/Item.java
+++ src/com/example/Demo_Subj/Item.java
@@ -2,32 +2,84 @@ package com.example.Demo_Subj;
 
 import android.graphics.Bitmap;
 
+import java.util.List;
+
 /**
  * Created by johannes on 24.11.2014.
- * Karst
+ * modified by Karsten Becker 10.12.2014
  */
 
 public class Item {
 
-    private Bitmap bitmapO;
+    private int active_state = 0; //state: 0= inactive ; 1=active by subject ; 2=active by user
+
+    private int id; //Item-ID
     private float xPos;
     private float yPos;
+    private String itemStandBitmapName; //name of resource to draw
+    private static int holdAnimationCycles = 20; //time between animation steps
 
-    public Item(Bitmap bitmap, float x, float y){
-        bitmapO = bitmap;
-        xPos = x;
+    private List<String> itemAnim;
+    private List<String> itemAnimInv;
+
+
+
+    //constructor
+    public Item(String itemStandBitmapName, int id, float x, float y){ //added id, abfangen, name der resource statt bitmap
+        this.itemStandBitmapName = itemStandBitmapName;
+        this.id = id;
+        //negative Werte für x und y abfangen
+        if(x >= 0.0f) {
+            xPos = x;
+        }
+        else{
+            xPos = 0.0f;
+        }
+        if(y >= 0.0f) {
+            yPos = x;
+        }
+        else{
+            yPos = 0.0f;
+        }
         yPos = y;
+
      }
 
+    //x-coordinate
     public float getxPos(){
-        return xPos;
+        return this.xPos;
     }
+    //y-coordinate
     public float getyPos(){
-        return yPos;
+        return this.yPos;
     }
-    public Bitmap getBitmapO(){
-        return bitmapO;
+    //returns name of resource to draw (instead of Bitmap)
+    public String getBitmap(){
+        return this.itemStandBitmapName;
+    }
+    //returns item-id
+    public int getID(){
+        return this.id;
     }
 
+    //item used by Subject
+    public void useBySubject(){
+        this.active_state = 1;
+
+    }
+    //item used by User
+    public void useByUser(){
+        this.active_state = 2;
+
+    }
+    //tick
+    public void tick(){
+
+        //TODO: active_state überprüfen
+    }
+    //adds drawable pictures for animation in a list
+    public void addResource(String resourcename){
+        itemAnim.add(resourcename);
+    }
 
 }
